a29671dd27d4558115abda5b0e97767af890ae26
Jay Lee <eggboy@gmail.com>
2024-12-11 11:58:04 +0800

    Exercise 5 - Step 02

--- src/ContosoSuitesWebAPI/Services/DatabaseService.cs
+++ src/ContosoSuitesWebAPI/Services/DatabaseService.cs
@@ -15,8 +15,9 @@ public class DatabaseService : IDatabaseService
     /// Get all hotels from the database.
     /// </summary>
 
-    [KernelFunction]
+    [KernelFunction("get_hotels")]
     [Description("Get all hotels")]
+
     public async Task<IEnumerable<Hotel>> GetHotels()
     {
         var sql = "SELECT HotelID, HotelName, City, Country FROM dbo.Hotel";
